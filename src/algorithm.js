0de7db1af167337f960248280fdce66a0c4e3815
Ian Holmes <ihh@berkeley.edu>
2020-03-30 11:57:51 -0700

    inching towards phylogenetic likelihood

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,3 +1,4 @@
+const mathjs = require ('mathjs');
 const models = require ('./models');
 
 const getRoots = (branches) => {
@@ -55,7 +56,12 @@ const indexBranchList = (branches) => {
     const branch = branchByChild[node]
     return [ preorderRank[branch[0]], preorderRank[branch[1]], branch[2] ]
   })
-  return { root, nodes, branches, children, descendants, branchLength, preorder, preorderRank, preorderBranches, distFromRoot, maxDistFromRoot }
+  const postorderBranches = preorderBranches.slice(0).reverse()
+  const leaves = nodes.filter ((node) => children[node].length == 0)
+  const internals = nodes.filter ((node) => children[node].length != 0)
+  const leafPreorderRank = leaves.map ((node) => preorderRank[node])
+  const internalPreorderRank = internals.map ((node) => preorderRank[node])
+  return { root, nodes, leaves, internals, branches, children, descendants, branchLength, preorder, preorderRank, preorderBranches, postorderBranches, leafPreorderRank, internalPreorderRank, distFromRoot, maxDistFromRoot }
 }
 
 const getNewickJSBranchList = (newickJS) => {
@@ -73,6 +79,56 @@ const getNewickJSBranchList = (newickJS) => {
 }
 
 const indexNewickJS = (newickJS) => indexBranchList (getNewickJSBranchList (newickJS));
+
+const indexAlphabet = (alphabet) => {
+  const chars = alphabet.split('')
+  const charToIndex = {}
+  chars.forEach ((c, i) => charToIndex[c] = i)
+  return {
+    size: alphabet.length,
+    chars,
+    charToIndex,
+    initForChar: chars.map ((c, i) => {
+      const a = new Float32Array (alphabet.length).fill(-Infinity);
+      a[i] = 0;
+      return a;
+    }),
+    initMissing: new Float32Array (alphabet.length).fill(0),
+    init: (c) => {
+      const i = charToIndex[c]
+      return typeof(i) === 'undefined' ? initMissing : initForChar[i]
+    }
+  }
+}
+
+const getMatrixExponentials = (model, treeIndex) => {
+  const chars = model.alphabet.split('')
+  const rateMatrix = chars.map ((ci, i) => chars.map (cj, j) => model.subrate[ci][cj] || 0)
+  chars.forEach ((ci, i) => {
+    rateMatrix[i][i] = 0;
+    chars.forEach (cj, j) => {
+      if (i != j)
+        rateMatrix[i][i] -= rateMatrix[i][j];
+    }
+  })
+  return treeIndex.preorderBranches.map ((branch) => mathjs.expm (mathjs.multiply (rateMatrix, branch[2])))
+}
+
+const initColumn = (treeIndex, alphabetIndex) => {
+  return new Array (treeIndex.nodes.length).fill(0)
+    .map (() => new Float32Array (alphabetIndex.size))
+}
+
+const logsumexp = (a, b) => Math.max (a, b) + Math.log (1 + Math.exp (-Math.abs (a - b)));
+
+const fillColumnUp = (treeIndex, alphabetIndex, columnChars, column) => {
+  const nodes = treeIndex.nodes.length, alphSize = alphabetIndex.size;
+  treeIndex.leafPreorderRank.forEach ((leaf) => column[leaf].set (alphabetIndex.init (columnChars[leaf])))
+  treeIndex.internalPreorderRank.forEach ((leaf) => column[leaf].set (alphabetIndex.initForMissing))
+  treeIndex.postorderBranches.forEach ((branch) => {
+    // ...
+  })
+}
 
 module.exports = { models,
                    indexNewickJS,
